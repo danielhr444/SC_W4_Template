5460e5acf21d400621631670f574b9a16005b9b6
danielhr444 <danielhr444@gmail.com>
2016-09-30 11:04:42 -0700

    Most of it done, one part confusing

---
+++ src/Commands/ArcadeDrive.cpp
@@ -0,0 +1,18 @@
+/*
+ * ArcadeDrive.cpp
+ *
+ *  Created on: Sep 30, 2016
+ *      Author: danie
+ */
+
+#include <Commands/ArcadeDrive.h>
+
+ArcadeDrive::ArcadeDrive() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ArcadeDrive::~ArcadeDrive() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Commands/ArcadeDrive.h
@@ -0,0 +1,17 @@
+/*
+ * ArcadeDrive.h
+ *
+ *  Created on: Sep 30, 2016
+ *      Author: danie
+ */
+
+#ifndef SRC_COMMANDS_ARCADEDRIVE_H_
+#define SRC_COMMANDS_ARCADEDRIVE_H_
+
+class ArcadeDrive {
+public:
+	ArcadeDrive();
+	virtual ~ArcadeDrive();
+};
+
+#endif /* SRC_COMMANDS_ARCADEDRIVE_H_ */
